aa5bddc99e1e86420f1964364db7890bb2e7633e
Chenzoh12 <scottivincentj@gmail.com>
2018-01-15 03:50:59 +0000

    End of cp7. My ionicons do not appear. I believe my link tag is correct and in the correct spot? I even copied the table exactly from example. When I change it to a script tag it recognizes the CDN but says there is an error in the sytax of the file.

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,53 +12,77 @@ class Album extends Component {
             
         });
         
-
-        
         this.state = {
-            
-            album: album
+            album: album,
+            currentSong: album.songs[0],
+            isPlaying: false
         };
+        
+        this.audioElement = document.createElement('audio');
+        this.audioElement.src = album.songs[0].audioSrc;
     };
+    
+    play() {
+        this.audioElement.play();
+        this.setState({ isPlaying: true });
+    }
+    
+    pause() {
+        this.audioElement.pause();
+        this.setState({ isPlaying: false });
+    }
+    
+    setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState({ currentSong: song });
+    }
+    
+    handleSongClick(song) {
+        const isSameSong = this.state.currentSong === song
+        if (this.state.isPlaying && isSameSong) {
+            this.pause();
+        } else {
+            if (!isSameSong) { this.setSong(song); }
+            this.play();
+        }
+    }
     
     render() {
         return (
             <section className="album">
                 <section id="album-info">
-                    <img id="album-cover-art" src={this.state.album.albumCover} alt="album cover"/>
+                    <img id="album-cover-art" src={this.state.album.albumCover} />
                     <div className="album-details">
-                         <h1 id="album-title">{this.state.album.title}</h1>
-                         <h2 className="artist">{this.state.album.artist}</h2>
-                         <div id="release-info">{this.state.album.releaseInfo}</div>
+                        <h1 id="album-title">{this.state.album.title}</h1>
+                        <h2 className="artist">{this.state.album.artist}</h2>
+                        <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
                     </div>
                 </section>
+                <table id="song-list">
                     <colgroup>
                         <col id="song-number-column" />
                         <col id="song-title-column" />
                         <col id="song-duration-column" />
-                    </colgroup>
-                    {
-
-                        this.state.album.songs.map( (song, index) => 
-                            <Link to={`/song/${song.slug}`} key={index}>
-    
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <Ionicon icon="md-play" color="purple"/>
-                                            <td>{index + 1}</td>
-                                            <td>{song.title}</td>
-                                            <td>{song.duration}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </Link>
-                            
-                        )
-                        
-                    }
+                    </colgroup>  
+                    <tbody>
+                        {this.state.album.songs.map( (song, index) => 
+                            <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                                <td className="song-actions">
+                                    <button>
+                                        <span className="song-number">{index+1}</span>
+                                        <span className="ion-play"></span>
+                                        <span className="ion-pause"></span>
+                                    </button>
+                                </td>
+                                <td className="song-title">{song.title}</td>
+                                <td className="song-duration">{song.duration}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </section>
-        );
+            );
+        }
     }
-}
 
 export default Album;
