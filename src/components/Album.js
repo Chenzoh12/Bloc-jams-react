e082126f10747517620252f75e5df87993d71c6c
Chenzoh12 <scottivincentj@gmail.com>
2018-01-15 03:12:51 +0000

    End of a6. Used the npmreact version of ionicons. What is usually the preferred method?

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import Ionicon from 'react-ionicons';
 
 class Album extends Component {
     constructor(props) {
         super(props);
-
  
         const album = albumData.find( album => {
             return album.slug === this.props.match.params.slug
+            
         });
         
-        this.state = { 
+
+        
+        this.state = {
+            
             album: album
         };
-    }
+    };
     
     render() {
         return (
             <section className="album">
                 <section id="album-info">
-                    <img id="album-cover-art" src={this.state.album.albumCover} />
+                    <img id="album-cover-art" src={this.state.album.albumCover} alt="album cover"/>
                     <div className="album-details">
                          <h1 id="album-title">{this.state.album.title}</h1>
                          <h2 className="artist">{this.state.album.artist}</h2>
                          <div id="release-info">{this.state.album.releaseInfo}</div>
                     </div>
                 </section>
-                <table id="song-list">
                     <colgroup>
                         <col id="song-number-column" />
                         <col id="song-title-column" />
                         <col id="song-duration-column" />
-                    </colgroup>  
-                    <tbody>
-                    </tbody>
-                </table>
+                    </colgroup>
+                    {
+
+                        this.state.album.songs.map( (song, index) => 
+                            <Link to={`/song/${song.slug}`} key={index}>
+    
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <Ionicon icon="md-play" color="purple"/>
+                                            <td>{index + 1}</td>
+                                            <td>{song.title}</td>
+                                            <td>{song.duration}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </Link>
+                            
+                        )
+                        
+                    }
             </section>
         );
     }
